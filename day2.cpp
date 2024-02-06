d867ffdb537c70910299bd121ba3b725fc6a1ee6
tau3 <smr.oznob@gmail.com>
2024-02-06 16:27:36 +0700

    solve day2_pt1

--- day2.cpp
+++ day2.cpp
@@ -1,3 +1,4 @@
+#include <fstream>
 #include <iostream>
 #include <ostream>
 #include <sstream>
@@ -7,11 +8,26 @@
 
 using namespace std;
 
-int solve(const vector<string> &lines);
+int solve_day2_pt1(const vector<string> &lines);
 bool parse_line(const string &line);
 vector<string> split(const string &line);
+bool is_possible(const string &color, int count);
 
 int main() {
+
+  vector<string> lines;
+  ifstream input("day2_input");
+  string line;
+  while (getline(input, line)) {
+    lines.push_back(line);
+  }
+
+  cout << "result = " << solve_day2_pt1(lines) << endl;
+
+  return 0;
+}
+
+void test() {
   const vector<string> lines = {
       "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
       "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
@@ -21,12 +37,10 @@ int main() {
       "red",
       "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"};
 
-  cout << "result = " << solve(lines) << ", expected = 8" << endl;
-
-  return 0;
+  cout << "result = " << solve_day2_pt1(lines) << ", expected = 8" << endl;
 }
 
-int solve(const vector<string> &lines) {
+int solve_day2_pt1(const vector<string> &lines) {
   int result = 0;
   for (size_t i = 0; i < lines.size(); ++i) {
     const bool possible = parse_line(lines[i]);
@@ -39,15 +53,19 @@ int solve(const vector<string> &lines) {
 
 bool parse_line(const string &line) {
   const vector<string> tokens = split(line);
-  for (size_t i = 2; i < tokens.size() - 1; ++i) {
+  for (size_t i = 2; i < tokens.size() - 1; i += 2) {
     const int count = stoi(tokens[i]);
     const string color = tokens[i + 1];
-    if (!(color[0] == 'r' && count <= 12) || (color[0] == 'g' && count <= 13) ||
-        (color[0] == 'b' && count <= 14)) {
+    if (!is_possible(color, count)) {
       return false;
     }
   }
   return true;
+}
+
+bool is_possible(const string &color, int count) {
+  return (color[0] == 'r' && count <= 12) || (color[0] == 'g' && count <= 13) ||
+         (color[0] == 'b' && count <= 14);
 }
 
 vector<string> split(const string &line) {

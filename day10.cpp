ebba54c10b38b29ede92edf2fb0a8019b2741d7b
tau3 <smr.oznob@gmail.com>
2024-02-21 15:00:42 +0800

    draft for pt2

--- day10.cpp
+++ day10.cpp
@@ -1,4 +1,7 @@
+#include <cstddef>
 #include <iostream>
+#include <queue>
+#include <set>
 #include <stdexcept>
 #include <string>
 #include <vector>
@@ -10,7 +13,7 @@ using namespace std;
 enum class Direction { UP, DOWN, LEFT, RIGHT };
 
 inline ostream &operator<<(ostream &os, const Direction &direction) {
-  switch(direction) {
+  switch (direction) {
   case Direction::UP:
     os << "Direction::UP";
     break;
@@ -144,11 +147,100 @@ int solve_pt1(const vector<string> &grid) {
   Direction direction = direction_for_start(grid, start);
 
   while ((current != start) || (steps == 0)) {
-    // cout << "current: " << current << ", direction: " << direction << endl;
     current.move(direction);
     ++steps;
     direction = pick_direction(grid, current, direction);
   }
 
   return steps / 2;
+}
+
+void replace_all(string &orig, string substring, string replacement) {
+  size_t pos = orig.find(substring);
+  while (pos != string::npos) {
+    orig.replace(pos, substring.size(), replacement);
+    pos = orig.find(substring, pos + replacement.size());
+  }
+}
+
+size_t count(const string &str, char symbol) {
+  size_t result = 0;
+  for (char c : str) {
+    if (c == symbol) {
+      ++result;
+    }
+  }
+  return result;
+}
+
+bool contains(const set<Point> &points, const Point &point) {
+  return points.find(point) != points.end();
+}
+
+bool inside(const vector<string> &grid, const set<Point> &loop,
+            const Point &point, const Point &start) {
+  // TODO may be wrong
+  string line;
+  const size_t from = (point.col < start.col) ? 0 : point.col + 1;
+  const size_t to = (point.col < start.col) ? point.col : grid[0].size();
+  for (size_t i = from; i < to; ++i) {
+    const Point current = Point(point.row, i);
+    const char value = grid[current.row][current.col];
+    if ((contains(loop, current)) && (value != '.') && (value != '-')) {
+      line += value;
+    }
+  }
+
+  replace_all(line, "F7", "");
+  replace_all(line, "LJ", "");
+  replace_all(line, "FJ", "|");
+  replace_all(line, "L7", "|");
+
+  return count(line, '|') % 2 == 1;
+}
+
+int foo(const vector<string> &grid) {
+  queue<Point> dots;
+  for (size_t row = 0; row < grid.size(); ++row) {
+    for (size_t col = 0; col < grid[0].size(); ++col) {
+      if (grid[row][col] == '.') {
+        dots.push(Point(row, col));
+      }
+    }
+  }
+
+  int result = 0;
+  while (!dots.empty()) {
+    Point point = dots.front();
+    dots.pop();
+
+    set<Point> visited;
+    queue<Point> neighbours;
+    neighbours.push(point);
+
+    while (!neighbours.empty()) {
+      point = neighbours.front();
+      neighbours.pop();
+      if (contains(visited, point)) {
+        continue;
+      }
+      const vector<Point> adjacent = point.adjacent();
+      neighbours.add_all(adjacent);
+    }
+
+    if (inside(grid, loop, point, start)) {
+      result += neighbours.size();
+    }
+
+    dots.remove_all(neighbours);
+  }
+  return result;
+}
+
+void solve_pt2(const vector<string> &grid) {
+  for (const string &line : grid) {
+    bool inside = false;
+    for (const char c : line) {
+    }
+  }
 }

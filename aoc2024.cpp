6b58c9de4af9dbc8b1771fa3cbd125d6f26cf2ec
sergey <sergey@terra>
2024-12-12 18:07:09 +0400

    initial day 4

--- aoc2024.cpp
+++ aoc2024.cpp
@@ -14,8 +14,9 @@ int main() {
   //   "8 6 4 4 1",
   //   "1 3 6 7 9",
   // };
-  const string input =
-      "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+  // const string input =
+      // "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+  const string input = read_file("../day3_input")[0];
   cout << solve(input) << endl;
 
   return 0;
--- day3.cpp
+++ day3.cpp
@@ -5,7 +5,6 @@
 #include <regex>
 #include <string>
 
-// mul(123,4)
 int calc_single_mul(const string &input) {
   std::regex mul("mul\\(([0-9]{1,3}),([0-9]{1,3})\\).*");
   std::smatch pieces_match;
@@ -19,12 +18,12 @@ int calc_single_mul(const string &input) {
 
 int solve(string input) {
   int result = 0;
-  size_t loc = input.find("mul(", 0);
-  while (loc < input.size()) {
+  size_t loc = 0;
+  while ((loc < input.size()) && (loc != string::npos)) {
+    loc = input.find("mul(", 0);
     input = input.substr(loc);
     result += calc_single_mul(input);
-    input = input.substr(4);
-    loc = input.find("mul(", 0);
+    input = input.substr(4); // "mul(".size()
   }
   return result;
 }

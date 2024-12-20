f4b2064ca3a77f70dc207b78f850f9bac111678e
tau3 <smr.oznob@gmail.com>
2024-12-20 20:00:40 +0400

    solving day 8

--- day8.cpp
+++ day8.cpp
@@ -1,21 +1,44 @@
 #include <cstddef>
-#include <functional>
 #include <string>
 #include <unordered_map>
+#include <unordered_set>
 #include <vector>
 
 using namespace std;
 
 using Point = pair<size_t, size_t>;
 
-vector<Point> antinodes(const Point &lhs, const Point &rhs,
-                        const Point &bound) {
-  vector<Point> result = {{lhs.first, rhs.second}, {lhs.second, lhs.first} };
-  
-  
+size_t distance(const Point &lhs, const Point &rhs) {
+  const size_t h =
+      lhs.first > rhs.first ? lhs.first - rhs.first : rhs.first - lhs.first;
+  const size_t w = lhs.second > rhs.second ? lhs.second - rhs.second
+                                           : rhs.second - lhs.second;
+  return h + w;
+};
+
+bool is_valid_antinode(const Point &antinode, const Point &lhs,
+                       const Point &rhs) {
+  const size_t lhs_distance = distance(antinode, lhs);
+  const size_t rhs_distnace = distance(antinode, rhs);
+  return (rhs_distnace / 2 == lhs_distance) ||
+         (lhs_distance / 2 == rhs_distnace);
 }
 
-void solve(const vector<string> &input) {
+vector<Point> find_antinodes(const Point &lhs, const Point &rhs,
+                        const Point &bound) {
+  vector<Point> result;
+  Point candidate = {lhs.first, rhs.second};
+  if (is_valid_antinode(candidate, lhs, rhs)) {
+    result.push_back(candidate);
+  }
+  candidate = {lhs.second, rhs.first};
+  if (is_valid_antinode(candidate, lhs, rhs)) {
+    result.push_back(candidate);
+  }
+  return result;
+}
+
+int solve(const vector<string> &input) {
   const size_t w = input[0].size();
   const size_t h = input.size();
 
@@ -37,13 +60,20 @@ void solve(const vector<string> &input) {
     }
   }
 
-  for(const auto& kv : antennas){
+  unordered_set<Point> result;
+  const Point bound = {h, w};
+  for (const auto &kv : antennas) {
     const vector<Point> points = kv.second;
-    for(size_t i =0; i< points.size(); ++i){
-      for(size_t j=i+1; j< points.size(); ++j){
-	const Point& lhs = points[i];
-	const Point& rhs = points[j];
+    for (size_t i = 0; i < points.size(); ++i) {
+      for (size_t j = i + 1; j < points.size(); ++j) {
+        const Point &lhs = points[i];
+        const Point &rhs = points[j];
+        const vector<Point> antinodes = find_antinodes(lhs, rhs, bound);
+        for (const Point &antinode : antinodes) {
+          result.insert(antinode);
+        }
       }
     }
   }
+  return result.size();
 }

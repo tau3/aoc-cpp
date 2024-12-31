b73a5d5e00efec2b60db1a4a07163d7d66b8cfe1
tau3 <smr.oznob@gmail.com>
2025-01-01 06:06:35 +0700

    solve day 17

--- aoc2024.cpp
+++ aoc2024.cpp
@@ -7,15 +7,15 @@ using namespace std;
 using namespace Day17;
 
 int main() {
-  // const vector<string> input = read_file("../day17_input");
+  const vector<string> input = read_file("../day17_input");
   // clang-format off
-  const vector<string> input = {
-"Register A: 729",
-"Register B: 0",
-"Register C: 0",
-"",
-"Program: 0,1,5,4,3,0",
-  };
+//   const vector<string> input = {
+// "Register A: 2024",
+// "Register B: 0",
+// "Register C: 0",
+// "",
+// "Program: 0,3,5,4,3,0",
+//   };
   // clang-format on
 
  cout << solve_day17_pt2(input) << endl;
--- day17.cpp
+++ day17.cpp
@@ -1,30 +1,27 @@
 #include "util.hpp"
 #include <cassert>
 #include <cmath>
-#include <cstddef>
-#include <ostream>
 #include <string>
 #include <tuple>
-#include <type_traits>
-#include <utility>
 #include <vector>
 
 namespace Day17 {
 
 using namespace std;
 
+using r_type = int64_t;
+static_assert(std::is_signed_v<r_type> == true);
+
 class Computer {
   Computer() {};
-  int a;
-  int b;
-  int c;
-  vector<int> out_stream;
-  int pointer;
-  vector<int> program;
+  r_type a;
+  r_type b;
+  r_type c;
+  vector<r_type> out_stream;
+  r_type pointer;
+  vector<r_type> program;
 
-  int eval_combo_operand(const int operand) {
-    assert(operand >= 0 && operand < 7);
-
+  r_type eval_combo_operand(const r_type operand) {
     switch (operand) {
     case 0:
     case 1:
@@ -42,13 +39,15 @@ class Computer {
     }
   }
 
-  void adv(const int operand) { a = a / (pow(2, eval_combo_operand(operand))); }
+  void adv(const r_type operand) {
+    a = a / (pow(2, eval_combo_operand(operand)));
+  }
 
-  void bxl(const int operand) { b ^= operand; }
+  void bxl(const r_type operand) { b ^= operand; }
 
-  void bst(const int operand) { b = eval_combo_operand(operand) % 8; }
+  void bst(const r_type operand) { b = eval_combo_operand(operand) % 8; }
 
-  void jnz(const int operand) {
+  void jnz(const r_type operand) {
     if (a == 0) {
       pointer += 2;
       return;
@@ -57,20 +56,22 @@ class Computer {
     pointer = operand;
   }
 
-  void bxc(const int operand) { b ^= c; }
+  void bxc(const r_type operand) { b ^= c; }
 
-  void out(const int operand) {
-    int result = eval_combo_operand(operand) % 8;
+  void out(const r_type operand) {
+    r_type result = eval_combo_operand(operand) % 8;
     out_stream.push_back(result);
   }
 
-  void bdv(const int operand) { b = a / (pow(2, eval_combo_operand(operand))); }
+  void bdv(const r_type operand) {
+    b = a / (pow(2, eval_combo_operand(operand)));
+  }
 
-  void cdv(const int operand) { c = a / (pow(2, eval_combo_operand(operand))); }
+  void cdv(const r_type operand) {
+    c = a / (pow(2, eval_combo_operand(operand)));
+  }
 
-  void exec(const int opcode, const int operand) {
-    assert(opcode >= 0 && opcode <= 7);
-
+  void exec(const r_type opcode, const r_type operand) {
     switch (opcode) {
     case 0:
       adv(operand);
@@ -103,25 +104,27 @@ class Computer {
       cdv(operand);
       pointer += 2;
       break;
+    default:
+      throw "unreachable";
     }
   }
 
 public:
-  Computer(const int a, const int b, const int c, const vector<int> program)
+  Computer(const r_type a, const r_type b, const r_type c,
+           const vector<r_type> program)
       : a(a), b(b), c(c), pointer(0), program(program) {}
 
   void run() {
     while (pointer < program.size()) {
       exec(program[pointer], program[pointer + 1]);
-      // dump_state();
     }
   }
 
-  const vector<int> &out_values() const { return out_stream; }
+  const vector<r_type> &out_values() const { return out_stream; }
 
   string output() const {
     string result = ",";
-    for (const int val : out_stream) {
+    for (const r_type val : out_stream) {
       result += to_string(val);
       result += ",";
     }
@@ -129,19 +132,19 @@ public:
   }
 };
 
-pair<tuple<int, int, int>, vector<int>>
+pair<tuple<r_type, r_type, r_type>, vector<r_type>>
 parse_input(const vector<string> &input) {
   const auto a_raw = split(input[0], " ");
   const auto b_raw = split(input[1], " ");
   const auto c_raw = split(input[2], " ");
   auto program_raw = split(input[4], " ");
 
-  int a = stoi(a_raw[2]);
-  int b = stoi(b_raw[2]);
-  int c = stoi(c_raw[2]);
+  r_type a = stol(a_raw[2]);
+  r_type b = stol(b_raw[2]);
+  r_type c = stol(c_raw[2]);
 
   program_raw = split(program_raw[1], ",");
-  vector<int> program;
+  vector<r_type> program;
   for (size_t i = 0; i < program_raw.size(); ++i) {
     program.push_back(stoi(program_raw[i]));
   }
@@ -158,28 +161,33 @@ string solve_day17_pt1(const vector<string> &input) {
   return computer.output();
 }
 
-int solve_day17_pt2(const vector<string> &input) {
+// stolen here: https://www.bytesizego.com/blog/aoc-day17-golang
+r_type solve_day17_pt2(const vector<string> &input) {
   const auto [registers, program] = parse_input(input);
-  auto [a, b, c] = registers;
-  a = 1;
+  auto [_, b, c] = registers;
+  r_type a = 1;
 
-  Computer comp(a, b, c, program);
-  comp.run();
-  vector<int> out = comp.out_values();
-
-  while (out.size() != program.size()) {
-    a *= 2;
+  while (true) {
     Computer comp(a, b, c, program);
     comp.run();
-    out = comp.out_values();
-  }
+    vector<r_type> out = comp.out_values();
+    if (out == program) {
+      return a;
+    }
 
-  for (int i = program.size() - 1; i >= 0; --i) {
-    if (program[i] != out[i]) {
-      a += pow(8, i);
+    if (out.size() < program.size()) {
+      a *= 2;
+      assert(a > 0);
+      continue;
+    }
+
+    for (r_type i = program.size() - 1; i >= 0; --i) {
+      if (program[i] != out[i]) {
+        a += pow(8, i);
+        break;
+      }
     }
   }
-  return a;
 }
 
 } // namespace Day17

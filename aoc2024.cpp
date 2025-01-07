02e9da49f8de170f45613aabc048edbfb53fb6d2
tau3 <smr.oznob@gmail.com>
2025-01-07 15:56:39 +0700

    solve day 22 pt1

--- aoc2024.cpp
+++ aoc2024.cpp
@@ -1,24 +1,23 @@
-#include "day21.cpp"
+#include "day22.cpp"
 #include "util.hpp"
 #include <iostream>
 #include <ostream>
 
 using namespace std;
-using namespace Day21;
+using namespace Day22;
 
 int main() {
-    const vector<string> input = read_file("../day21_input");
-    // clang-format off
-//   const vector<string> input = {
-// "029A",
-// "980A",
-// "179A",
-// "456A",
-// "379A",
-//   };
+  const vector<string> input = read_file("../day22_input");
+  // clang-format off
+  // const vector<string> input = {
+  //   "1",
+  //   "10",
+  //   "100",
+  //   "2024",
+  // };
   // clang-format on
 
-  cout << solve_day21_pt1(input) << endl;
+  cout << solve_day22_pt1(input) << endl;
 
   return 0;
 }

dbaf10c7edd3c74585f7f10d886dfedab14a575b
tau3 <smr.oznob@gmail.com>
2025-01-07 16:25:07 +0700

    solve pt2 - initial

--- day22.cpp
+++ day22.cpp
@@ -1,4 +1,7 @@
+#include <array>
 #include <string>
+#include <unordered_map>
+#include <unordered_set>
 #include <vector>
 
 namespace Day22 {
@@ -37,6 +40,66 @@ num_t solve_day22_pt1(const vector<string> &input) {
     result += generate(secret);
   }
   return result;
+}
+
+using Cache = unordered_map<array<int, 4>, int>;
+
+Cache foo(const num_t secret) {
+  vector<int> prices;
+  prices.reserve(2000 + 1);
+  prices[0] = secret;
+
+  for (int i = 1; i < 2000; ++i) {
+    prices[i] = generate(prices[i - 1]);
+    prices[i] = prices[i] % 10;
+  }
+
+  Cache cache;
+  for (size_t i = 3; i < prices.size(); ++i) {
+    const int t0 = prices[i - 3];
+    const int t1 = prices[i - 2];
+    const int t2 = prices[i - 1];
+    const int t3 = prices[i];
+
+    const array<int, 4> key = {t0, t1, t2, t3};
+    cache[key] = max(prices[i], cache[key]);
+  }
+  return cache;
+}
+
+int solve_day22_pt2(const vector<string> &input) {
+  vector<Cache> caches;
+  caches.reserve(input.size());
+  for (const string &line : input) {
+    const Cache cache = foo(stol(line));
+    caches.push_back(cache);
+  }
+
+  unordered_set<array<int, 4>> all_keys;
+  for (const Cache &cache : caches) {
+    for (const auto &[k, v] : cache) {
+      all_keys.insert(k);
+    }
+  }
+
+  unordered_map<array<int, 4>, int> result;
+  for (const array<int, 4> &key : all_keys) {
+    for (const Cache &cache : caches) {
+      int price = 0;
+      if (cache.find(key) != cache.end()) {
+        price = cache.at(key);
+      }
+      result[key] += price;
+    }
+  }
+
+  int x = 0;
+  for (const auto &[k, v] : result) {
+    if (x < v) {
+      x = v;
+    }
+  }
+  return x;
 }
 
 } // namespace Day22

bb6f06bd28ceec27a33a82b9c22177bb8f523a9b
tau3 <smr.oznob@gmail.com>
2025-12-02 04:27:15 +0400

    prepare pt2

--- day1.cpp
+++ day1.cpp
@@ -1,11 +1,14 @@
 #include "day1.hpp"
 #include <iostream>
+#include <string>
 
 namespace Day1 {
 
-int apply(const int, const string &);
+int apply_pt1(const int, const string &);
+int apply_pt2(const int, const string &);
 
-int solve_day1_pt1(const vector<string> &rotations) {
+int solve(const vector<string> &rotations,
+          int (*apply)(const int, const string &)) {
   int current = 50;
   int result = 0;
   for (const string &rotation : rotations) {
@@ -18,14 +21,32 @@ int solve_day1_pt1(const vector<string> &rotations) {
   return result;
 }
 
-int apply(const int current, const string &rotation) {
+int solve_day1_pt1(const vector<string> &rotations) {
+  return solve(rotations, apply_pt1);
+}
+
+int solve_day1_pt2(const vector<string> &rotations) {
+  return solve(rotations, apply_pt2);
+}
+
+int apply_pt1(const int current, const string &rotation) {
   int amount = stoi(rotation.substr(1));
-  // if (amount == 99) {
-    // return current;
-  // }
-  
   const int direction = (rotation[0] == 'L') ? -1 : 1;
-  amount %= 99;
+  amount %= 100;
+  amount *= direction;
+  int result = current + amount;
+  if (result < 0) {
+    result = 99 + result + 1;
+  } else if (result > 99) {
+    result = result - 99 - 1;
+  }
+  return result;
+}
+
+int apply_pt2(const int current, const string &rotation) {
+  int amount = stoi(rotation.substr(1));
+  const int direction = (rotation[0] == 'L') ? -1 : 1;
+  amount %= 100;
   amount *= direction;
   int result = current + amount;
   if (result < 0) {
--- day1.hpp
+++ day1.hpp
@@ -8,6 +8,7 @@ namespace Day1 {
 using namespace std;
 
 int solve_day1_pt1(const vector<string> &rotations);
+int solve_day1_pt2(const vector<string> &rotations);
 } // namespace Day1
 
 #endif

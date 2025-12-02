c9a9b7e570e40c19ea55fde54bda7d3c80895d5a
tau3 <smr.oznob@gmail.com>
2025-12-02 04:19:10 +0400

    fixing execution

--- aoc2024.cpp
+++ aoc2024.cpp
@@ -1,34 +1,27 @@
-#include "day16.hpp"
+#include "day1.hpp"
 #include "util.hpp"
 
 using namespace std;
-using namespace Day16;
+using namespace Day1;
 
 int main() {
-  const vector<string> input = read_file("../day16_input");
+  // const vector<string> input = read_file("../day16_input");
   // clang-format off
-//   const vector<string> input = {
-// "#################",
-// "#...#...#...#..E#",
-// "#.#.#.#.#.#.#.#.#",
-// "#.#.#.#...#...#.#",
-// "#.#.#.#.###.#.#.#",
-// "#...#.#.#.....#.#",
-// "#.#.#.#.#.#####.#",
-// "#.#...#.#.#.....#",
-// "#.#.#####.#.###.#",
-// "#.#.#.......#...#",
-// "#.#.###.#####.###",
-// "#.#.#...#.....#.#",
-// "#.#.#.#####.###.#",
-// "#.#.#.........#.#",
-// "#.#.#.#########.#",
-// "#S#.............#",
-// "#################",
-// };
+  const vector<string> input = {
+    "L68",
+    "L30",
+    "R48",
+    "L5",
+    "R60",
+    "L55",
+    "L1",
+    "L99",
+    "R14",
+    "L82",
+};
   // clang-format on
 
-  cout << solve_day16_pt1(input) << endl;
+  cout << solve_day1_pt1(input) << endl;
 
   return 0;
 }
--- day1.cpp
+++ day1.cpp
@@ -1,4 +1,5 @@
 #include "day1.hpp"
+#include <iostream>
 
 namespace Day1 {
 
@@ -9,6 +10,7 @@ int solve_day1_pt1(const vector<string> &rotations) {
   int result = 0;
   for (const string &rotation : rotations) {
     current = apply(current, rotation);
+    cout << rotation << ' ' << current << endl;
     if (current == 0) {
       ++result;
     }
@@ -17,12 +19,21 @@ int solve_day1_pt1(const vector<string> &rotations) {
 }
 
 int apply(const int current, const string &rotation) {
-  const int direction = (rotation[0] == 'L') ? -1 : 1;
   int amount = stoi(rotation.substr(1));
+  // if (amount == 99) {
+    // return current;
+  // }
+  
+  const int direction = (rotation[0] == 'L') ? -1 : 1;
   amount %= 99;
   amount *= direction;
   int result = current + amount;
-  return result %= 99;
+  if (result < 0) {
+    result = 99 + result + 1;
+  } else if (result > 99) {
+    result = result - 99 - 1;
+  }
+  return result;
 }
 
 } // namespace Day1

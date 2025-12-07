ef62ebe54e42684c60195f972e2c0fd9ffdffa9b
tau3 <smr.oznob@gmail.com>
2025-12-07 07:52:05 +0400

    solve day6pt1

--- day6.cpp
+++ day6.cpp
@@ -8,30 +8,21 @@
 
 namespace Day6 {
 
-void check_sizes(const vector<vector<string>> &tokenized) {
-  const size_t a = tokenized[0].size();
-  const size_t b = tokenized[1].size();
-  const size_t c = tokenized[2].size();
-  const size_t d = tokenized[3].size();
-  assert((a == b) && (a == c) && (a == d));
-}
 uint64_t solve_day6_pt1(const vector<string> &input) {
   vector<vector<string>> tokenized;
   for (const string &line : input) {
     vector<string> tokens = util::split_by_spaces(line);
     tokenized.push_back(tokens);
   };
-  check_sizes(tokenized);
   uint64_t result = 0;
   for (size_t i = 0; i < tokenized[0].size(); i++) {
-    uint64_t a = stoi(tokenized[0][i]);
-    uint64_t b = stoi(tokenized[1][i]);
-    uint64_t c = stoi(tokenized[2][i]);
-    if (tokenized[3][i] == "+") {
-      result += (a + b + c);
-    } else {
-      result += (a * b * c);
+    const string operation = tokenized[tokenized.size() - 1][i];
+    uint64_t subresult = (operation == "+") ? 0 : 1;
+    for (size_t j = 0; j < tokenized.size() - 1; j++) {
+      const uint64_t num = stoi(tokenized[j][i]);
+      subresult = (operation == "+") ? (subresult + num) : (subresult * num);
     }
+    result += subresult;
   }
   return result;
 }

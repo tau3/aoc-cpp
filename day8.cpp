0b13c3f49d84a863d0e59f7211b0fbd8608e29d6
tau3 <smr.oznob@gmail.com>
2025-12-08 13:25:11 +0400

    speedup

--- day8.cpp
+++ day8.cpp
@@ -3,7 +3,6 @@
 #include <algorithm>
 #include <array>
 #include <cmath>
-#include <cstddef>
 #include <map>
 #include <string>
 #include <unordered_set>
@@ -39,15 +38,6 @@ struct CircuitSizeComparator {
   }
 };
 
-size_t total_connections(const vector<Circuit> &circuits) {
-  size_t result = 0;
-  for (const Circuit &circuit : circuits) {
-    const size_t size = circuit.size();
-    result += (size == 1) ? 0 : size - 1;
-  }
-  return result;
-}
-
 long solve_day8_pt1(const vector<string> &input) {
   Distances distances;
   for (size_t i = 0; i < input.size(); i++) {
@@ -60,14 +50,16 @@ long solve_day8_pt1(const vector<string> &input) {
   }
 
   vector<Circuit> circuits;
-  while (total_connections(circuits) < 1000) {
+  int total_pairs = 0;
+  while (total_pairs < 1000) {
     const auto &[left, right] = distances.erase(distances.begin())->second;
     bool is_new_circuit = true;
     for (Circuit &circuit : circuits) {
       if ((circuit.find(left) != circuit.end()) ||
           (circuit.find(right) != circuit.end())) {
-        circuit.emplace(left);
-        circuit.emplace(right);
+        if (circuit.emplace(left).second || circuit.emplace(right).second) {
+          total_pairs++;
+        };
         is_new_circuit = false;
         break;
       }
@@ -77,13 +69,9 @@ long solve_day8_pt1(const vector<string> &input) {
       circuit.emplace(left);
       circuit.emplace(right);
       circuits.push_back(circuit);
+      total_pairs++;
     }
   }
-
-  // for (const Circuit &circuit : circuits) {
-  //   cout << circuit.size() << " ";
-  // }
-  // cout << endl;
 
   sort(circuits.begin(), circuits.end(), CircuitSizeComparator());
   return circuits[0].size() * circuits[1].size() * circuits[2].size();

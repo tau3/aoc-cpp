c6e14528910bfe71451cffa35820d0b93389f4f5
tau3 <smr.oznob@gmail.com>
2025-12-15 10:14:32 +0700

    day10pt2 draft

--- day10.cpp
+++ day10.cpp
@@ -1,24 +1,37 @@
 #include "day10.hpp"
+#include "gauss.hpp"
 #include "util.hpp"
 #include <cassert>
 #include <queue>
+#include <tuple>
 #include <unordered_set>
 #include <vector>
 
 namespace Day10 {
 
-void unquote(string &str) {
+string &unquote(string &str) {
   str.erase(0, 1);
   str.erase(str.size() - 1);
+  return str;
 }
 
 using Button = vector<size_t>;
 using State = vector<bool>;
+using Levels = vector<size_t>;
 
 State press_button(const State &state, const Button &button) {
   State result = state;
   for (const size_t i : button) {
     result[i] = !state[i];
+  }
+  return result;
+}
+
+vector<size_t> string_to_nums(const string &input) {
+  vector<size_t> result;
+  const vector<string> tokens = util::split(input, ",");
+  for (const string &token : tokens) {
+    result.push_back(stol(token));
   }
   return result;
 }
@@ -33,27 +46,26 @@ State parse_target(string &target) {
   return result;
 }
 
-pair<State, vector<Button>> parse_line(const string &line) {
+tuple<State, vector<Button>, Levels> parse_line(const string &line) {
   vector<string> tokens = util::split(line, " ");
+
   vector<Button> buttons;
   for (size_t i = 1; i < tokens.size() - 1; i++) {
     string &button_str = tokens[i];
     unquote(button_str);
+    const Button button = string_to_nums(button_str);
     const vector<string> indexes = util::split(button_str, ",");
-    Button button;
-    for (const string &index : indexes) {
-      button.push_back(stol(index));
-    }
     buttons.push_back(button);
   }
 
   State target = parse_target(tokens[0]);
+  Levels levels = string_to_nums(unquote(tokens[tokens.size() - 1]));
 
-  return {target, buttons};
+  return {target, buttons, levels};
 }
 
 int solve_line(const string &line) {
-  const auto &[target, buttons] = parse_line(line);
+  const auto &[target, buttons, _] = parse_line(line);
 
   queue<pair<State, int>> states;
   const auto initial = vector(target.size(), false);
@@ -88,6 +100,42 @@ int solve_day10_pt1(const vector<string> &input) {
   int result = 0;
   for (const string &line : input) {
     result += solve_line(line);
+  }
+  return result;
+}
+
+vector<vector<Gauss::num_t>> to_matrix(const vector<Button> &buttons,
+                                       const Levels &levels) {
+  vector<vector<Gauss::num_t>> result;
+  for (size_t i = 0; i < levels.size(); i++) {
+    vector<Gauss::num_t> line;
+    for (const Button &button : buttons) {
+      Gauss::num_t val =
+          (find(button.begin(), button.end(), i) != button.end()) ? 1 : 0;
+      line.push_back(val);
+    }
+    line.push_back(levels[i]);
+    result.push_back(line);
+  }
+  return result;
+}
+
+int solve_pt2_line(const string &line) {
+  const auto &[_, buttons, levels] = parse_line(line);
+  auto matrix = to_matrix(buttons, levels);
+  const vector<Gauss::num_t> solution = Gauss::gauss(matrix);
+
+  int result = 0;
+  for (size_t i = 0; i < solution.size(); i++) {
+    result += solution[i];
+  }
+  return result;
+}
+
+int solve_day10_pt2(const vector<string> &input) {
+  int result = 0;
+  for (const string &line : input) {
+    result += solve_pt2_line(line);
   }
   return result;
 }

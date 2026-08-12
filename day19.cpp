8ae276ebbfc9c2cf54cbac0c0f857414e02a12df
tau3 <smr.oznob@gmail.com>
2026-08-12 13:33:30 +0400

    rewrite solution

--- day19.cpp
+++ day19.cpp
@@ -40,74 +40,56 @@ size_t solve_day19_pt1(const vector<string> &input) {
   return solve(replacements, molecule);
 }
 
-using Graph = unordered_map<string, vector<string>>;
-
-void insert_edge(Graph &graph, const string &from, const string &to) {
-  if (graph.find(from) != graph.end()) {
-    graph[from].push_back(to);
-  } else {
-    graph[from] = {to};
-  }
-}
-
-struct State {
-  string m;
-  int depth;
-  vector<string> variants;
-};
-
-vector<string> make_all_replacements(const vector<string> &vs,
-                                     const string &key, const string &value) {
-  vector<string> result;
+unordered_set<string> make_all_replacements(const unordered_set<string> &vs,
+                                            const string &key,
+                                            const string &value) {
+  unordered_set<string> result;
   for (const auto &molecule : vs) {
     size_t from = molecule.find(key, 0);
     while (from != molecule.npos) {
       auto copy = molecule;
       copy.replace(from, key.size(), value);
-      result.push_back(copy);
+      result.emplace(copy);
       from = molecule.find(key, from + key.size());
     }
   }
   return result;
 }
 
+void add_all(unordered_set<string> &left, const unordered_set<string> &right) {
+  for (const auto &r : right) {
+    left.emplace(r);
+  }
+}
+
 size_t solve_day19_pt2(const vector<string> &input) {
-  Graph graph;
+  Replacements replacements;
   for (size_t i = 0; i < input.size() - 2; i++) {
     const auto tokens = util::split(input[i], " => ");
-    const auto from = tokens[0];
-    const auto to = tokens[1];
-    insert_edge(graph, from, to);
+    replacements.push_back({tokens[0], tokens[1]});
   }
 
   const auto molecule = input[input.size() - 1];
 
-  deque<State> q;
-  unordered_set<string> visited;
-
-  q.push_back({"e", 0, {"e"}});
-  while (!q.empty()) {
-    const auto &[v, d, vs] = q.front();
-    q.pop_front();
-
-    const auto &adjacent = graph.at(v);
-    for (const auto &w : adjacent) {
-
-      vector<string> variants = make_all_replacements(vs, v, w);
-      for (const auto &variant : variants) {
-        if (variant == molecule) {
-          cout << "FOUND" << endl;
-          return 0;
+  unordered_set<string> variants = {"e"};
+  size_t i = 0;
+  while (true) {
+    unordered_set<string> new_variants;
+    for (const auto &[from, to] : replacements) {
+      unordered_set<string> new_variants_1 =
+          make_all_replacements(variants, from, to);
+      for (const auto &v : new_variants_1) {
+        cout << v << endl;
+        if (v == molecule) {
+          return i + 1;
         }
       }
-
-      if (visited.find(w) == visited.end()) {
-        visited.emplace(w);
-        q.push_back({w, d + 1, variants});
-      }
+      add_all(new_variants, new_variants_1);
+      cout << i << endl;
     }
+    i++;
+    variants = new_variants;
   }
-
   throw runtime_error("unreachable");
 }
 

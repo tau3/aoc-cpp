6ab2b5f0d77d475aa8b22783cbc80b8eb85ffe32
tau3 <smr.oznob@gmail.com>
2026-08-13 12:19:36 +0400

    parallel execution

--- day20.cpp
+++ day20.cpp
@@ -1,7 +1,18 @@
 #include "day20.hpp"
+#include <climits>
+#include <future>
 #include <iostream>
+#include <mutex>
+#include <stdexcept>
+#include <vector>
 
 namespace Day20 {
+
+using namespace std;
+
+const int THREADS = 10;
+
+std::mutex m;
 
 int calc_presents(const int house_number) {
   int result = 1 + house_number;
@@ -13,18 +24,33 @@ int calc_presents(const int house_number) {
   return result * 10;
 }
 
-int solve_day20_pt1(const int presents) {
-  int i = 1000000;
-  while (true) {
+int calc_block(int from_inclusive, int size, int presents) {
+  for (int i = from_inclusive; i < from_inclusive + size; i++) {
     const int current = calc_presents(i);
-    if (current < presents) {
-      i *= 2;
-    } else if (current > presents) {
-      i /= 2;
+    if (current == presents) {
+      return i;
     }
-    std::cout << i << " " << current << " " << presents << std::endl;
-    i++;
   }
+  return -1;
+}
+
+int solve_day20_pt1(const int presents) {
+  const int block_size = INT_MAX / THREADS;
+
+  vector<future<int>> futures;
+  for (int i = 0; i < THREADS; i++) {
+    std::future<int> future =
+        std::async(calc_block, i * block_size, block_size, presents);
+    futures.push_back(std::move(future));
+  }
+
+  for (future<int> &future : futures) {
+    const int result = future.get();
+    if (result != -1) {
+      return result;
+    }
+  }
+  throw runtime_error("failure");
 }
 
 } // namespace Day20

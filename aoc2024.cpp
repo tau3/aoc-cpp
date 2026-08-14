e902711ac604f93b96b3a0fa0d9b4d11fbfcf655
tau3 <smr.oznob@gmail.com>
2026-08-14 14:02:34 +0400

    add items

--- aoc2024.cpp
+++ aoc2024.cpp
@@ -1,11 +1,12 @@
-#include "day20.hpp"
+#include "day21.hpp"
 #include "util.hpp"
 #include <iostream>
+#include <optional>
 #include <ostream>
 #include <vector>
 
 using namespace std;
-using namespace Day20;
+using namespace Day21;
 
 int main() {
   // const vector<string> input = util::read_file("../day19_input");
@@ -23,5 +24,14 @@ int main() {
 
   // cout << solve_day19_pt2(input) << endl;
 
-  cout << solve_day20_pt1(29000000) << endl;
+  // cout << solve_day20_pt1(29000000) << endl;
+
+  Weapon weapon = Weapon("Shortsword", 10, 5, 0);
+  Armor armor = Armor("Platemail", 102, 0, 5);
+  Player player = Player(8, weapon, armor, nullopt, nullopt);
+  Boss boss = Boss(12, 7, 2);
+
+  cout << fight(&player, &boss) << endl;
+
+  return 0;
 }
--- day21.hpp
+++ day21.hpp
@@ -2,18 +2,22 @@
 #define INCLUDE_DAY_21_H
 
 #include <optional>
+#include <stdexcept>
 #include <string>
+#include <vector>
 
 namespace Day21 {
 
 using namespace std;
 
 class Item {
+protected:
   string name;
   int cost;
   int damage;
   int armor;
 
+private:
   Item() {};
 
 public:
@@ -45,7 +49,50 @@ public:
   explicit Ring(const string &name, const int cost, const int damage,
                 const int armor)
       : Item(name, cost, damage, armor) {};
+
+  // TODO move from class
+  inline bool operator==(const Ring &other) const {
+    return name == other.name && cost == other.cost && damage == other.damage &&
+           armor == other.armor;
+  }
 };
+
+vector<Weapon> weapons() {
+  static vector<Weapon> weapons = {
+      Weapon("Dagger", 8, 4, 0),
+      Weapon("Shortsword", 10, 5, 0),
+      Weapon("Warhammer", 25, 6, 0),
+      Weapon("Longsword", 40, 7, 0),
+  };
+  return weapons;
+}
+
+vector<Armor> armors() {
+  // clang-format off
+  static vector<Armor> armors = {
+      Armor("Leather", 13, 0, 1),
+      Armor("Chainmail", 31, 0, 2),
+      Armor("Splintmail", 53, 0, 3),
+      Armor("Bandedmail", 75, 0, 4),
+      Armor("Platemail", 102, 0, 5),
+  };
+  // clang-format on
+  return armors;
+}
+
+vector<Ring> rings() {
+  // clang-format off
+  static vector<Ring> rings = {
+      Ring("Damage +1", 25, 1, 0),
+      Ring("Damage +2", 50, 2, 0),
+      Ring("Damage +3", 100, 3, 0),
+      Ring("Defense +1", 20, 0, 1),
+      Ring("Defense +2", 40, 0, 2),
+      Ring("Defense +3", 80, 0, 3),
+  };
+  // clang-format on
+  return rings;
+}
 
 class Warrior {
 private:
@@ -60,7 +107,7 @@ public:
 
   inline bool is_alive() const { return hp >= 0; }
 
-  Warrior(int hp) : hp(hp) {};
+  explicit Warrior(int hp) : hp(hp) {};
   Warrior(const Warrior &) = delete;
   Warrior &operator=(const Warrior &) = delete;
   virtual ~Warrior() = default;
@@ -75,6 +122,18 @@ class Player : public Warrior {
   Player() = delete;
 
 public:
+  explicit Player(const int hp, const optional<Weapon> &weapon,
+                  const optional<Armor> &armor, const optional<Ring> &left_ring,
+                  const optional<Ring> &right_ring)
+      : Warrior(hp), weapon(weapon), armor(armor), left_ring(left_ring),
+        right_ring(right_ring) {
+    if (left_ring.has_value() && right_ring.has_value()) {
+      if (left_ring.value() == right_ring.value()) {
+        throw runtime_error("repeated ring");
+      }
+    }
+  };
+
   inline int get_damage() const override {
     int result = 0;
     if (weapon) {
@@ -117,6 +176,8 @@ class Boss : public Warrior {
   Boss() = delete;
 
 public:
+  explicit Boss(const int hp, const int damage, const int armor)
+      : Warrior(hp), damage(damage), armor(armor) {}
   int get_damage() const override { return damage; }
 
   int get_armor() const override { return armor; }

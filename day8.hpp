bb741d7bf287037da70cb3809ce49d0b052dc847
tau3 <smr.oznob@gmail.com>
2026-09-03 12:28:32 +0400

    day9 - initial

---
+++ day8.hpp
@@ -0,0 +1,15 @@
+#ifndef INCLUDE_DAY_8_H
+#define INCLUDE_DAY_8_H
+
+#include <string>
+#include <vector>
+
+namespace Day8 {
+
+using namespace std;
+
+size_t solve_day8_pt1(const vector<string> &input);
+
+} // namespace Day8
+
+#endif
---
+++ day9.cpp
@@ -0,0 +1,62 @@
+#include <cstddef>
+#include <queue>
+#include <string>
+
+using namespace std;
+
+enum class State {
+  NONE,
+  MARKER_PREFIX,
+  MARKER_SUFFIX,
+  MARKER_EXIT,
+};
+
+void repeat(string &prefix, const string &suffix, const size_t count);
+
+void decompress(const string &str) {
+  State state = State::NONE;
+  string result = "";
+  string prefix = "";
+  string suffix = "";
+  for (size_t i = 0; i < str.size(); i++) {
+    const char c = str[i];
+    switch (c) {
+    case '(':
+      state = State::MARKER_PREFIX;
+      continue;
+    case 'x':
+      state = State::MARKER_SUFFIX;
+      continue;
+    case ')':
+      state = State::MARKER_EXIT;
+      continue;
+    };
+
+    switch (state) {
+    case State::NONE:
+      result += c;
+      break;
+    case State::MARKER_PREFIX:
+      prefix += c;
+      break;
+    case State::MARKER_SUFFIX:
+      suffix += c;
+      break;
+    case State::MARKER_EXIT:
+      const size_t subsequent = stol(prefix);
+      const size_t repeats = stol(suffix);
+      const string repeated = str.substr(i, i + subsequent);
+      repeat(result, repeated, repeats);
+      suffix = "";
+      prefix = "";
+      i += repeats;
+      break;
+    }
+  }
+}
+
+void repeat(string &prefix, const string &suffix, const size_t count) {
+  for (size_t i = 0; i < count; i++) {
+    prefix += suffix;
+  }
+}
